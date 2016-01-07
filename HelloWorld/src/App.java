a016b13d04dda4fce98f6397182d143c9b39f61e
Hivernus <edgarpombal@gmail.com>
2016-01-07 20:45:29 +0000

    HelloWorld Project

---
+++ HelloWorld/src/App.java
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * @author formando
+ *
+ */
+public class App 
+{
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main (String[] args)
+	{
+		System.out.println("Hello World");	
+	}
+
+}
