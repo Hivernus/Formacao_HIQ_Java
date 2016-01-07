194b58c94e6c5c88c79ffebd92ef67e756bc3c1a
Hivernus <edgarpombal@gmail.com>
2016-01-07 21:41:21 +0000

    Edgar

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -12,7 +12,8 @@ public class App
 	 */
 	public static void main (String[] args)
 	{
-		System.out.println("Hello World");	
+		System.out.println("Hello World");
+		System.out.println("Edgar");
 	}
 
 }
