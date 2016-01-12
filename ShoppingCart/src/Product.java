27e400b593c3a6fe233e0475757d7b38a3a7284e
Hivernus <edgarpombal@gmail.com>
2016-01-12 20:05:37 +0000

    Alterações

---
+++ ShoppingCart/src/Product.java
@@ -0,0 +1,18 @@
+
+public class Product {
+	
+	// classe -> definição de um tipo
+	// objecto -> instância de uma classe
+	
+	// declaração dos atributos da classe
+	
+	/*
+	 * Attributes
+	 */
+	
+	int customer_id;
+	String name;
+	float price; //float -> 32 bits e double -> 64 bits
+	
+	
+}
---
+++ ShoppingCart/src/ShoppingCart.java
@@ -0,0 +1,10 @@
+
+public class ShoppingCart {
+/*
+ * Attributes
+ */
+	
+	float total;
+	boolean finish;
+	
+}
